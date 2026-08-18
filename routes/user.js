10d3ff376225b00b9033774471d8e77fbdecbf8b
Pawan Awasthi <pawankumarawasthi@Pawans-MacBook-Air.local>
2026-08-18 14:37:37 +0530

    fix case sensitive model imports for deployment

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../models/user.js");
+const User = require("../Models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 
 const passport = require("passport");
